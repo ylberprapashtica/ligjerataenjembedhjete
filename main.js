fa31fbae376fff34f2d3bb4e04d42234a673e55c
Ylber Prapashtica <ylberprapashtica@gmail.com>
2024-01-14 19:45:11 +0000

    Add PErsonal infomration

---
+++ main.js
@@ -0,0 +1,10 @@
+var emri = "Ylber";
+var mbiemri = "Prapashtica";
+var mosha = "26";
+
+var emri2 = "Gent";
+var mbiemri2 = "Zarari"
+var mosha2 = "25";
+
+console.log(emri, mbiemri, mosha);
+console.log(Emri, mbiemri2, mosha2);
