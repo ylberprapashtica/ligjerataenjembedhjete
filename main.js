96bf8b9fd10e3122aa0438c5de6dd5c02bb69b6e
Ylber Prapashtica <ylberprapashtica@gmail.com>
2024-01-14 20:05:16 +0000

    Introduction to objects

--- main.js
+++ main.js
@@ -1,10 +1,48 @@
-var emri = "Ylber";
-var mbiemri = "Prapashtica";
-var mosha = "26";
+//Gjinia true i bjen Femen, gjinia false i bjen Mashkull
 
-var emri2 = "Gent";
-var mbiemri2 = "Zarari"
-var mosha2 = "25";
+var personi1 = {
+    emri: "Anlya",
+    mbiemri: "Kabashi",
+    mosha: 28,
+    gjinia: true,
+    addresa: {
+        rruga: "Rruga B",
+        numer: "21"
+    }
+}
 
-console.log(emri, mbiemri, mosha);
-console.log(Emri, mbiemri2, mosha2);
+var personi2 = {
+    emri: "Gent",
+    mbiemri: "Zarari",
+    mosha: 25,
+    gjinia: false,
+    addresa: {
+        rruga: "Rruga B",
+        numer: "22"
+    }
+}
+
+var personi3 = {
+    emri: "Yiber",
+    mbiemri: "Prapashtica",
+    mosha: 26,
+    gjinia: false,
+    addresa: {
+        rruga: "Rruga C",
+        numer: "8"
+    }
+}
+
+console.log(personi1.emri, personi1.mbiemri, personi1.mosha, personi1.gjinia);
+console.log(personi2.emri, personi2.mbiemri, personi2.mosha, personi2.gjinia);
+console.log(personi3.emri, personi3.mbiemri, personi3.mosha, personi3.gjinia);
+
+personi3.emri = "Ylber";
+
+console.log(personi3.emri, personi3.mbiemri, personi3.mosha);
+
+console.log("Adersa e " + personi1.emri + " " + personi1.mbiemri + " eshte " + 
+personi1.addresa.rruga + ", " + personi1.addresa.numer
+)
+
+console.log(personi1.gjatess);
